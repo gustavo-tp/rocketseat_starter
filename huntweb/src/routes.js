6b4908356e8e386236de083f90620e06dab491e7
gustavo-tp <gustavo1190.gt@gmail.com>
2020-04-08 11:11:13 -0300

    trying to deploy 14

--- huntweb/src/routes.js
+++ huntweb/src/routes.js
@@ -6,12 +6,12 @@ import Main from './pages/main';
 import Product from './pages/product';
 
 const Routes = () => (
-    <BrowserRouter>
-        <Switch>
-            <Route exact path="/" component={Main} />
-            <Route path="/products/:id" component={Product} />
-        </Switch>
-    </BrowserRouter>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Main} />
+      <Route path="/products/:id" component={Product} />
+    </Switch>
+  </BrowserRouter>
 );
 
 export default Routes;
