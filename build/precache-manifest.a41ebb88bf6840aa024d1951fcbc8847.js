e2d74267e782955030158d4a08f8a5113c3bef2a
gustavo-tp <gustavo1190.gt@gmail.com>
2020-04-08 03:25:46 -0300

    trying to deploy 8

---
+++ build/precache-manifest.a41ebb88bf6840aa024d1951fcbc8847.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbae90a4a4469d729428153443f23738",
+    "url": "/index.html"
+  },
+  {
+    "revision": "628772fad8459a645386",
+    "url": "/static/css/main.20e357a5.chunk.css"
+  },
+  {
+    "revision": "b89259456451506bf364",
+    "url": "/static/js/2.eb8b58b3.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/static/js/2.eb8b58b3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "628772fad8459a645386",
+    "url": "/static/js/main.75a28254.chunk.js"
+  },
+  {
+    "revision": "e36cc1a78616bb9d40ea",
+    "url": "/static/js/runtime-main.9d863bc3.js"
+  }
+]);
