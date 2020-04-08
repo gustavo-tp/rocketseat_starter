1d027d4d4096632ab65e43c3d325dbbc78d52f08
gustavo-tp <gustavo1190.gt@gmail.com>
2020-04-08 02:43:31 -0300

    Updates

---
+++ precache-manifest.88819dae72485b0aef1f989cfc048e1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b3707713939fa135f9117e2d2fc3ad6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdd2974e05552421300c",
+    "url": "/static/css/main.20e357a5.chunk.css"
+  },
+  {
+    "revision": "b89259456451506bf364",
+    "url": "/static/js/2.eb8b58b3.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/static/js/2.eb8b58b3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bdd2974e05552421300c",
+    "url": "/static/js/main.aa08006c.chunk.js"
+  },
+  {
+    "revision": "e36cc1a78616bb9d40ea",
+    "url": "/static/js/runtime-main.9d863bc3.js"
+  }
+]);
